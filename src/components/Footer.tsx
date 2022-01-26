821c00c52b9b2bb4c87f77baf3dcf48e8ec90d9c
Yuto Tachibana <tachibanayu24@gmail.com>
2022-01-26 14:35:46 +0900

    Tweak Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,38 +2,40 @@ import { VFC } from "react";
 
 export const Footer: VFC = () => {
   return (
-    <footer className="p-4 font-light text-xs flex justify-center items-center">
-      <div className="border-solid border-r-2 border-slate-300 dark:border-slate-600 pr-4 h-full w-1/2 text-right">
-        DevToolkit v0.0.0
-      </div>
-      <div className="pl-4 w-1/2 text-left">
-        <a
-          href="https://github.com/tachibanayu24/dev-toolkit/releases"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-red-400 font-bold mr-2"
-        >
-          Source Code
-        </a>
-        <a
-          href="https://github.com/tachibanayu24/dev-toolkit/releases"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-red-400 font-bold"
-        >
-          Release Note
-        </a>
-        <p className="mt-1">
-          Copyright © MIT by{" "}
+    <footer className="m-4 text-xs">
+      <div className="flex justify-center items-center">
+        <div className="pr-4 h-full text-right">DevToolkit v0.0.0</div>
+        <div className="pl-4 text-left border-solid border-l-2 border-slate-300 dark:border-slate-600">
           <a
-            href="https://tachibanayu24.com/"
+            href="https://github.com/tachibanayu24/dev-toolkit/issues/new"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-red-400 font-bold"
+            className="text-red-400 font-bold block mb-2"
           >
-            Yuto Tachibana
+            Feature Request
           </a>
-        </p>
+
+          <a
+            href="https://github.com/tachibanayu24/dev-toolkit/releases"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-red-400 font-bold block mb-2"
+          >
+            Release Note
+          </a>
+
+          <p>
+            Copyright © MIT by{" "}
+            <a
+              href="https://tachibanayu24.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-red-400 font-bold"
+            >
+              Yuto Tachibana
+            </a>
+          </p>
+        </div>
       </div>
     </footer>
   );
