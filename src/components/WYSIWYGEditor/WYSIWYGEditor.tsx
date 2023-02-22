bc2a963e2bb599c833dff3f28466fe8a38eb8c9e
Yuto Tachibana <tachibanayu24@gmail.com>
2023-02-23 05:12:15 +0900

    WIP: Create Editor component

---
+++ src/components/WYSIWYGEditor/WYSIWYGEditor.tsx
@@ -0,0 +1,39 @@
+import { useEditor, EditorContent } from "@tiptap/react";
+import { Node } from "@tiptap/core";
+import StarterKit from "@tiptap/starter-kit";
+
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import Heading from "@tiptap/extension-heading";
+
+import style from "./style.module.css";
+
+export const WYSIWYGEditor = () => {
+  const CustomNode = Node.create({
+    // Hea,
+  });
+
+  const editor = useEditor({
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      Heading.configure({ levels: [1, 2, 3] }),
+    ],
+    content: `<h1>Heading1</h1>
+<h2>Heading2</h2>
+<h3>Heading3</h3>
+<p>paragpraph</p>
+
+`,
+    autofocus: "end",
+  });
+
+  return (
+    <>
+      <div className={style.div}>testhogehgoe</div>
+      <EditorContent className={style.editor} editor={editor} />
+    </>
+  );
+};
