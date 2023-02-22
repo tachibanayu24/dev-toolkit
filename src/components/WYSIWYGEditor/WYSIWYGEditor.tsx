b99ca95dd041df6e7ba44cb9c5fa2f2560b5eb96
Yuto Tachibana <tachibanayu24@gmail.com>
2023-02-23 05:14:22 +0900

    Blockquote

--- src/components/WYSIWYGEditor/WYSIWYGEditor.tsx
+++ src/components/WYSIWYGEditor/WYSIWYGEditor.tsx
@@ -6,6 +6,7 @@ import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
 import Heading from "@tiptap/extension-heading";
+import Blockquote from "@tiptap/extension-blockquote";
 
 import style from "./style.module.css";
 
@@ -20,12 +21,15 @@ export const WYSIWYGEditor = () => {
       Paragraph,
       Text,
       Heading.configure({ levels: [1, 2, 3] }),
+      Blockquote,
     ],
     content: `<h1>Heading1</h1>
 <h2>Heading2</h2>
 <h3>Heading3</h3>
 <p>paragpraph</p>
-
+<blockquote>
+  Nothing is impossible, the word itself says “I’m possible!”
+</blockquote>
 `,
     autofocus: "end",
   });
