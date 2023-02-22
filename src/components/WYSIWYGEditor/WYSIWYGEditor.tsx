df7f5e4e7f230adae1e4be70f6f3f8a3d268cdbd
Yuto Tachibana <tachibanayu24@gmail.com>
2023-02-23 05:26:41 +0900

    Highlighting code area

--- src/components/WYSIWYGEditor/WYSIWYGEditor.tsx
+++ src/components/WYSIWYGEditor/WYSIWYGEditor.tsx
@@ -9,8 +9,20 @@ import Heading from "@tiptap/extension-heading";
 import Blockquote from "@tiptap/extension-blockquote";
 import BulletList from "@tiptap/extension-bullet-list";
 import ListItem from "@tiptap/extension-list-item";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { lowlight } from "lowlight";
+
+import css from "highlight.js/lib/languages/css";
+import js from "highlight.js/lib/languages/javascript";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
 
 import style from "./style.module.css";
+
+lowlight.registerLanguage("html", html);
+lowlight.registerLanguage("css", css);
+lowlight.registerLanguage("js", js);
+lowlight.registerLanguage("ts", ts);
 
 export const WYSIWYGEditor = () => {
   const CustomNode = Node.create({
@@ -26,6 +38,9 @@ export const WYSIWYGEditor = () => {
       Blockquote,
       BulletList,
       ListItem,
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
     ],
     content: `<h1>Heading1</h1>
 <h2>Heading2</h2>
@@ -42,6 +57,18 @@ export const WYSIWYGEditor = () => {
   <li>A list item</li>
   <li>And another one</li>
 </ol>
+
+<pre><code class="language-javascript">for (var i=1; i <= 20; i++)
+{
+  if (i % 15 == 0)
+    console.log("FizzBuzz");
+  else if (i % 3 == 0)
+    console.log("Fizz");
+  else if (i % 5 == 0)
+    console.log("Buzz");
+  else
+    console.log(i);
+}</code></pre>
 `,
     autofocus: "end",
   });
